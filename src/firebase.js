6a55183971b324f5a63e34a4e943be8e96fcd11f
Colombia-cyber <dlrhayward@hotmail.com>
2025-09-07 10:11:46 +1000

    Add firebase configuration and initialization

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV4g50dUdMJ6-a2R6MFqzL1JElgG51d8g",
+  authDomain: "nuestro-pulso-chat.firebaseapp.com",
+  projectId: "nuestro-pulso-chat",
+  storageBucket: "nuestro-pulso-chat.appspot.com",
+  messagingSenderId: "268702824909",
+  appId: "1:268702824909:web:0b4f2f849201abc94cac84",
+  measurementId: "G-74ZBHG8TF4"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { app, analytics, auth };
