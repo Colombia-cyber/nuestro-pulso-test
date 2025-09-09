6a919d6af6c71c4afd8033e59ffb6469a2130a75
Colombia-cyber <dlrhayward@hotmail.com>
2025-09-09 16:36:21 +1000

    Add Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <div className="pt-20">
+        <HeroSection />
+      </div>
+    </div>
+  );
+}
