3f30485d30eba1f00dfec5a62ec511becca65e5f
Colombia-cyber <dlrhayward@hotmail.com>
2025-09-10 15:03:42 +1000

    Update tailwind.config.js with new configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{tsx,ts,jsx,js}",
+    "./pages/**/*.{tsx,ts,jsx,js}",
+    "./components/**/*.{tsx,ts,jsx,js}",
   ],
   theme: {
     extend: {},
