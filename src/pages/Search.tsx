4314210b50962b1c65420a3e98f5f32ffed02d8f
Colombia-cyber <dlrhayward@hotmail.com>
2025-09-13 14:52:28 +1000

    Add dedicated universal search page with GoogleWebSearchBar

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import GoogleWebSearchBar from "../components/GoogleWebSearchBar";
 
 const SearchPage: React.FC = () => (
-  <main style={{ padding: "2rem", maxWidth: 700, margin: "0 auto" }}>
-    <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>Búsqueda Universal</h1>
+  <div>
+    <h1>Búsqueda Universal</h1>
     <GoogleWebSearchBar />
-    {/* Room for future: filters, history, trending, etc. */}
-  </main>
+    {/* Optionally add filters, advanced options, etc. */}
+  </div>
 );
 
 export default SearchPage;
