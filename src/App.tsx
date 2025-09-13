17b001e37bcab336b344029f5058dfac4fa5aadd
Colombia-cyber <dlrhayward@hotmail.com>
2025-09-13 14:52:42 +1000

    Integrate GoogleWebSearchBar in App for fallback universal access

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,8 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* Universal fallback: visible on all routes if navbar isn't enough */}
-      <div style={{ padding: "1rem", maxWidth: 600, margin: "0 auto" }}>
-        <GoogleWebSearchBar />
-      </div>
+      {/* Universal fallback: visible on all routes */}
+      <GoogleWebSearchBar />
       {/* ...rest of your app, routes, sections... */}
     </div>
   );
