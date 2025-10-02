b2bb88b66dd92dfbbe2e7066b803edee893a5e6b
Colombia-cyber <dlrhayward@hotmail.com>
2025-10-03 01:48:30 +1000

    Add ReelsSection component

---
+++ src/components/ReelsSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReelsSection: React.FC = () => {
+    return (
+        <div className="reels-section">
+            <h2>Reels</h2>
+            {/* Insert reels content here */}
+        </div>
+    );
+};
+
+export default ReelsSection;
